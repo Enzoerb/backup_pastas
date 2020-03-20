4fb9235ed11f2726f2dfeb16b8fe0d32672c76be
Enzoerb <33969883+Enzoerb@users.noreply.github.com>
2020-03-20 12:17:12 -0300

    arruma erro

--- gera_estrutura.sh
+++ gera_estrutura.sh
@@ -1,7 +1,7 @@
 PARAM="$1"
 
 for (( i=1; i<11; i++ )); do
-  mkdir ${PARAM}/dir_${i}
+  mkdir -p ${PARAM}/dir_${i}
   for (( j=1; j<=i; j++ )); do
     touch ${PARAM}/dir_${i}/arquivo${j}.txt
   done
