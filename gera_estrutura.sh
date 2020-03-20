cd1f337d9de573d4c6c373258ab8773ce196bf06
Enzoerb <33969883+Enzoerb@users.noreply.github.com>
2020-03-20 11:21:47 -0300

    atualização gera_estrutura
    permite com que estruturas sejam geradas sem que endereço necessite ser descrito desde a raiz

--- gera_estrutura.sh
+++ gera_estrutura.sh
@@ -1,10 +1,16 @@
 ARVORE="$1"
+PRIMEIRO_CARACTERE=${ARVORE::1}
 ARVORE=$(echo $ARVORE | tr "/" "\n")
-PASTA_ATUAL=""
+
+if [ $PRIMEIRO_CARACTERE == "/" ]; then
+  PASTA_ATUAL="/"
+else
+  PASTA_ATUAL=""
+fi
 
 for PASTA in $ARVORE
 do
-  PASTA_ATUAL="$PASTA_ATUAL/$PASTA"
+  PASTA_ATUAL="${PASTA_ATUAL}${PASTA}/"
   
   if [ ! -e "${PASTA_ATUAL}" ]; then
     mkdir "${PASTA_ATUAL}"
