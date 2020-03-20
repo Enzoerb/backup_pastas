46507f992c4e0de0ddd33067dd8c97e82d88eab3
Enzoerb <33969883+Enzoerb@users.noreply.github.com>
2020-03-20 12:55:32 -0300

    cria outro backup caso ja exista um com mesmo nome

--- backup.sh
+++ backup.sh
@@ -5,16 +5,29 @@ if [ "${PASTA_ATUAL: -1}" == "/" ]; then
 fi
 
 if [ ! -e "/${PASTA_ATUAL}_backup" ]; then
-  mkdir "/${PASTA_ATUAL}_backup"
+  PASTA_BACKUP="/${PASTA_ATUAL}_backup"
+else
+  i=2
+  for (( i=2; 0 == 0; i++ )); do
+    if [ ! -e "/${PASTA_ATUAL}_backup${i}" ]; then
+      PASTA_BACKUP="/${PASTA_ATUAL}_backup${i}"
+      break
+    fi
+  done
+  
 fi
+mkdir "$PASTA_BACKUP"
 
 exception="$PASTA_ATUAL/target"
 PASTAS_FOR=${PASTA_ATUAL}/*
 
 for arquivo in $PASTAS_FOR
 do
+  
   if [ "$arquivo" != "$exception" ]; then 
+    
     tar -czf "${arquivo}.tar.gz" "$arquivo"
-    mv "${arquivo}.tar.gz" "/${PASTA_ATUAL}_backup"
+    mv "${arquivo}.tar.gz" "${PASTA_BACKUP}"
+
   fi 
 done
