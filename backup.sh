1a06012c60b9ed9597203951842c06de2c665d5d
Enzoerb <33969883+Enzoerb@users.noreply.github.com>
2020-03-24 09:26:45 -0300

    fixing backup paste format

--- backup.sh
+++ backup.sh
@@ -24,15 +24,15 @@ function format_folder_adress ()
 function create_backup_adress ()
 {
 
-  if [ ! -e "\${MAIN_FOLDER}_backup" ] 
+  if [ ! -e "/${MAIN_FOLDER}_backup" ] 
   then
-    BACKUP_FOLDER="\${MAIN_FOLDER}_backup"
+    BACKUP_FOLDER="/${MAIN_FOLDER}_backup"
   else
     for (( i=2; 0 == 0; i++ )); do
 
-      if [ ! -e "\${MAIN_FOLDER}_backup(${i})" ]
+      if [ ! -e "/${MAIN_FOLDER}_backup(${i})" ]
       then
-        BACKUP_FOLDER="\${MAIN_FOLDER}_backup(${i})"
+        BACKUP_FOLDER="/${MAIN_FOLDER}_backup(${i})"
         break
       fi
 
